7bcc0bd82f415da3db81a597d726ef4f3229cf51
Trent_D <tdickson91@gmail.com>
2022-06-07 20:37:47 -0400

    markdown pseudocode

---
+++ utils/markdown.js
@@ -0,0 +1,22 @@
+function markdown(data) {
+    return ``;
+
+    // data.title
+
+    // data.describe
+
+    // data.URL
+
+    // data.about
+
+    // data.install
+
+    // data.userName
+
+    // data.email
+
+    // data.license
+
+    // data.repository
+
+}
