4dcbc5f6d8b3096f964110be334ff5214d73fc12
Trent_D <tdickson91@gmail.com>
2022-06-09 17:37:17 -0400

    syntax and spelling

--- utils/questions.js
+++ utils/questions.js
@@ -1,19 +1,18 @@
-// fs required?
-const questions = [
+let questions = [
     {
-      type: "input",
-      name: "title",
-      message: "What is the title of your project?"
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
     },
     {
-      type: "input",
-      name: "describe",
-      message: "Enter a single sentence description for your project."
+        type: "input",
+        name: "describe",
+        message: "Enter a single sentence to describe your project."
     },
     {
         type: "input",
         name: "about",
-        message: "What is your project and what is the scope of it?"
+        message: "What is your project about?"
     },
     {
         type: "input",
@@ -22,13 +21,24 @@ const questions = [
     },
     {
         type: "input",
-        name: "test",
-        message: "Are there any testing protocols related to your project?"
+        name: "clone",
+        message: "What is the link to clone your repository?"
+    },
+    {
+        type: "list",
+        name: "licenceBadge",
+        message: "Please select the license you used for this project.",
+        choices: [
+          "Mozilla",
+          "MIT",
+          "Apache",
+          "Boost",
+        ]
     },
     {
         type: "input",
-        name: "author",
-        message: "Who worked on this project?"
+        name: "test",
+        message: "Are there any testing protocols related to this project?"
     },
     {
         type: "input",
@@ -42,24 +52,19 @@ const questions = [
     },
     {
         type: "input",
-        name: "URL",
-        message: "What is the URL of the live site?"
+        name: "website",
+        message: "What is the hyperlink to the website?"
     },
     {
         type: "input",
         name: "repository",
-        message: "What is the URL to the Github repository?"
+        message: "What is the hyperlink to the Github repository?"
     },
     {
-        type: "list",
-        name: "license",
-        message: "Please select the license you used for this project.",
-        choices: [
-          "Mozilla",
-          "MIT",
-          "Apache",
-        ]
-    }
+        type: "input",
+        name: "author",
+        message: "Who worked on this project?"
+    },
   ];
 
 module.exports = {
