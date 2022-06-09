260ed51de5a9455f332090f4bda5e710b4f02dde
Trent_D <tdickson91@gmail.com>
2022-06-09 17:37:07 -0400

    markdown framework, modularization

--- utils/markdown.js
+++ utils/markdown.js
@@ -1,30 +1,86 @@
+// https://www.markdownguide.org/basic-syntax/
 function markdown(data) {
     return `
-    
+
 # ${data.title}
 
     ${data.describe}
 
-    ${data.license}
+    ${data.licenceBadge}
+
+    [Find the link to live website here:](${data.website})
+
+---
+## Table of Contents
+
+1. [About!](#about)
+    a. [Story](#story)
+    b. [Criteria](#criteria)
+    c. [Technology](#technology)
+2. [Installation](#installation)
+3. [Licence](#licence)
+4. [Contribution](#contribution)
+5. [Tests](#tests)
+6. [Authors](#authors)
+
+---
+## About 
+
+    ${data.about}
+
+---
+## Story
+
+---
+## Criteria
+
+---
+## Technology
+
+---
+## Installation
+
+    ${data.installation}
+
+    Clone the repository:
+
+        git clone ${data.clone}
+
+---
+## Licence
+
+    Licence used: ${data.licence}
+
+---
+## Contribution
+
+    If you would like to learn how to assist or contribute to this project:
+        - Fork the repository
+        - Create a feature branch (git checkout -b NAME)
+        - Commit your new feature (git commit -m "feature")
+        - Push your feature branch (git push)
+        - Create a new pull request
+
+---
+## Tests
+
+    ${data.test}
+
+---
+## Authors
+
+    ${data.author}
+
+---
+## Contact
+
+> GitHub username: ${data.userName}
+
+> Contact email: ${data.email}
 
     `;
-
-    // data.title
-
-    // data.describe
-
-    // data.URL
-
-    // data.about
-
-    // data.install
-
-    // data.userName
-
-    // data.email
-
-    // data.license
-
-    // data.repository
-
 }
+
+module.exports = {
+    markdown: markdown,
+};
