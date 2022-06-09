70c89827d6724a34edb14b67c43aa87618a5c88e
Trent_D <tdickson91@gmail.com>
2022-06-09 17:47:28 -0400

    pseudocode

---
+++ index.js
@@ -0,0 +1,15 @@
+// variables
+// file system
+// inquirer
+// utilities
+// markdown
+// licences
+// questions
+// promise to write file for README
+// asynchronous function to run the markdownFunction
+// answers variable?
+// licence badge
+// README creator
+// write README file
+// catch and throw errors
+// run the async function
