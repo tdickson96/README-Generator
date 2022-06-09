c421af9356c9618e390076322729cde34e512c11
Trent_D <tdickson91@gmail.com>
2022-06-09 17:36:38 -0400

    licence with a C!

---
+++ utils/licences.js
@@ -0,0 +1,16 @@
+// List of badges for licensing copy and paste
+function licences(badge) {
+  if (value === "Apache") {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (value === "Boost") {
+    return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  } else if (value === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } 
+}
+
+module.exports = {
+  licences: licences,
+};
