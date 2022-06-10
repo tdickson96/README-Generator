9dd9f048eff1ad54b5a3ba87a9a231438db7ee17
Trent_D <tdickson91@gmail.com>
2022-06-09 20:47:10 -0400

    fully working async function and fixing variables

--- index.js
+++ index.js
@@ -9,19 +9,24 @@ const { markdownFunction } = require("./utils/markdown");
 // markdown
 let markdown = require("./utils/markdown");
 // licences
-let licenceBadge = require("./utils/licenceBadges").badge;
+let licenceBadge = require("./utils/licenceBadges").licenceBadge;
 // questions
 let questions = require("./utils/questions").questions;
 // promise to write file for README
 let writeFileAsyncFunction = util.promisify(fs.writeFile);
 
 // asynchronous function to run the markdownFunction
+// Main idea of async function is to let the rest of the code to run while this function is running
+// Synchronous needs to finish before any other code can run (writing a file in Word)
+// Async needs to run other code before it needs to finish (do this at the end but before the promisify)
 async function generateReadme() {
     try {
         // answers variable?
         let answers = await inquirer.prompt(questions);
-        // licence badge
+        // licence 
         answers.licences = licenceBadge(answers.licenceBadge);
+        // licence badge
+        console.log(answers.licences);
         // README creator
         let readmeInfo = markdownFunction(answers);
         // write README file
@@ -32,5 +37,5 @@ async function generateReadme() {
     }
 }
 
-// run the async
+// run the async fxn
 generateReadme();
